78c367bb6b56c1804eaa0f49f140feec468a2c8a
pedro <pedro.henrique2512@hotmail.com>
2022-02-02 17:14:40 -0300

    Added subnet

--- aula.tf
+++ aula.tf
@@ -14,7 +14,28 @@ provider "aws" {
     #secret_key = 
 }
 
-#resource "aws_instance" "helloworld"{
-    #ami = "ami-04505e74c0741db8d"
+resource "aws_vpc" "vpc_brq" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+      Name = "VPC_legal"
+  }
+}
+
+resource "aws_subnet" "subrede_brq" {
+  vpc_id     = aws_vpc.vpc_brq.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "subrede_legal"
+  }
+}
+
+# resource "aws_instance" "helloworld"{
+#     ami = "ami-04505e74c0741db8d"
 #    instance_type = "t2.micro"
-#}
+
+#     tags = {
+#         Name = "Robson"
+#     }
+
+# }
